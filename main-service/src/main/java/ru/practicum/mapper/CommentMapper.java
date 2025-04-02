27a003729a3c2210adf9771d06bbc0518564150c
numerouno_life <dollari.91@mail.ru>
2025-04-02 10:21:42 +0400

    исправил маппер, исправил случайный перенос строки в schema.sql

--- main-service/src/main/java/ru/practicum/mapper/CommentMapper.java
+++ main-service/src/main/java/ru/practicum/mapper/CommentMapper.java
@@ -2,6 +2,7 @@ package ru.practicum.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import ru.practicum.dtos.comment.CommentDto;
 import ru.practicum.dtos.comment.NewCommentDto;
 import ru.practicum.model.Comment;
@@ -10,11 +11,12 @@ import ru.practicum.service.comment.CommentService;
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
 
-    @Mapping(target = "authorName", source = "userId", qualifiedByName = "getUserNameById")
-    CommentDto mapToDto(Comment comment);
+    @Mapping(target = "authorName", expression = "java(commentService.getUserNameById(comment.getUserId()))")
+    CommentDto mapToDto(Comment comment, CommentService commentService);
 
     Comment mapToComment(NewCommentDto newCommentDto);
 
+    @Named("getUserNameById")
     default String getUserNameById(Long userId, CommentService commentService) {
         return commentService.getUserNameById(userId);
     }
